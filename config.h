0a88f02d71e39b453d4f519c874ac4e9c45ef242
Christophe de Dinechin <dinechin@redhat.com>
2017-05-02 15:39:45 +0200

    Add check for functions using a configuration file check

---
+++ config.h
@@ -0,0 +1,7 @@
+#define HAVE_STDIO_H 1
+#define HAVE_UNISTD_H 1
+/* #undef HAVE_NONEXISTENT_H */
+#define HAVE_SYS_TIME_H 1
+/* #undef HAVE_SYS_IMPROBABLE_H */
+#define HAVE_IOSTREAM 1
+/* #undef HAVE_CLEARENV_H */
