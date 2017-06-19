a7e9b464d9c822c8650fb44c2748b68ba82df0b8
Christophe de Dinechin <dinechin@redhat.com>
2017-06-19 12:59:10 +0200

    Add arm-linux-gnu cross-compiler support
    (No support for selecting include and library path yet)

---
+++ config.arm-linux-gnu.mk
@@ -0,0 +1,23 @@
+#******************************************************************************
+# config.arm-linux-gnu.mk                                        Build project 
+#******************************************************************************
+#
+#  File Description:
+#
+#    Compilation profile for arm-linux-gnu
+#
+#
+#
+#
+#
+#
+#
+#
+#******************************************************************************
+# (C) 2017 Christophe de Dinechin <christophe@dinechin.org>
+#This software is licensed under the GNU General Public License v3
+#See file COPYING for details.
+#******************************************************************************
+
+CROSS_COMPILE=arm-linux-gnu
+include $(BUILD)config.gnu.mk
--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,9 @@
 #include "config.h"
+#if HAVE_IOSTREAM
 #include <iostream>
+#elif HAVE_STDIO_H
+#include <stdio.h>
+#endif // IO
 
 #if HAVE_SYS_IMPROBABLE_H
 #warning "Improbable header present, probably a fault in the build system"
@@ -11,5 +15,11 @@
 
 int main()
 {
+#if HAVE_IOSTREAM
     std::cout << "You successfully built using build\n";
+#elif HAVE_STDIO_H
+    printf("You successfully built using build (without iostream)\n");
+#else
+#warning "Building without <iostream> or <stdio.h>. Cross-compiling?"
+#endif // HAVE_IOSTREAM
 }
