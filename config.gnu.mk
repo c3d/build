49adc492f5469c11beccbd43c8b3bfd1f1ef581f
Christophe de Dinechin <dinechin@redhat.com>
2017-11-27 18:55:58 +0100

    Do not add LDFLAGS when building static libraries

--- config.gnu.mk
+++ config.gnu.mk
@@ -74,7 +74,7 @@ MAKE_CC=        $(CC)   $(CFLAGS)   $(CPPFLAGS_$*) $(CFLAGS_$*)   -c $< -o $@ $(
 MAKE_CXX=       $(CXX)  $(CXXFLAGS) $(CPPFLAGS_$*) $(CXXFLAGS_$*) -c $< -o $@ $(DEPFLAGS)
 MAKE_AS=        $(CC)   $(CFLAGS)   $(CPPFLAGS_$*) $(CFLAGS_$*)   -c $< -o $@ $(DEPFLAGS)
 MAKE_OBJDIR=    mkdir -p $*                                       && touch $@
-MAKE_LIB=       $(AR) $@         $(LDFLAGS) $(LDFLAGS_$*)  $(LINK_INPUTS)&& $(RANLIB) $@
+MAKE_LIB=       $(AR) $@         $(LINK_INPUTS)&& $(RANLIB) $@
 MAKE_DLL=       $(LD) -shared    $(LDFLAGS) $(LDFLAGS_$*)  $(LINK_CMDLINE)         -o $@
 MAKE_EXE=       $(LD)            $(LDFLAGS) $(LDFLAGS_$*)  $(LINK_CMDLINE)         -o $@
 
